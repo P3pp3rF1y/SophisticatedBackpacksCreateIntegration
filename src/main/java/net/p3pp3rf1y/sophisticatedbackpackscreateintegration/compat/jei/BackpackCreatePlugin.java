9026f207e41cc2c35a3fb7851dbae28a7535a778
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-05-26 08:53:36 +0200

    fix: 🐛 Fixed scrolling functionality of settings templates buttons when used with emi / rei

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpackscreateintegration/compat/jei/BackpackCreatePlugin.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpackscreateintegration/compat/jei/BackpackCreatePlugin.java
@@ -48,7 +48,7 @@ public class BackpackCreatePlugin implements IModPlugin {
 		registration.addGuiContainerHandler(MountedBackpackSettingsScreen.class, new IGuiContainerHandler<>() {
 			@Override
 			public List<Rect2i> getGuiExtraAreas(MountedBackpackSettingsScreen gui) {
-				return new ArrayList<>(gui.getSettingsTabControl().getTabRectangles());
+				return new ArrayList<>(gui.getExtendedControlsRectangles());
 			}
 		});
 
