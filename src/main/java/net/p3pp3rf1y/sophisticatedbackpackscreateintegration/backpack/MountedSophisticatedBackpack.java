7c9dfc03ffe94277a306f7e3dddb35cde85e72c2
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-10-30 21:59:06 +0100

    feat: ✨ Updated with support for Create 6.0.7+

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpackscreateintegration/backpack/MountedSophisticatedBackpack.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpackscreateintegration/backpack/MountedSophisticatedBackpack.java
@@ -55,7 +55,7 @@ public class MountedSophisticatedBackpack extends MountedStorageBase {
 			MountedSophisticatedBackpack::new, MountedSophisticatedBackpack::getStorageStack
 	).fieldOf("value");
 
-	private IBackpackWrapper backpackWrapper = IBackpackWrapper.Noop.INSTANCE;;
+	private IBackpackWrapper backpackWrapper = IBackpackWrapper.Noop.INSTANCE;
 
 	@Nullable
 	private WeakReference<Entity> contraptionEntity = null;
@@ -104,20 +104,16 @@ public class MountedSophisticatedBackpack extends MountedStorageBase {
 		contraptionEntity = new WeakReference<>(entity);
 	}
 
-	@Override
-	protected void afterInitialSync() {
-		updateRenderAttributes = true;
-	}
-
 	private void refreshRenderBlockEntity() {
 		Entity e = getEntity();
 		if (e instanceof AbstractContraptionEntity abstractContraptionEntity
-				&& abstractContraptionEntity.getContraption().presentBlockEntities.get(localPos) instanceof BackpackBlockEntity backpackBe) {
+				&& abstractContraptionEntity.getContraption().getBlockEntityClientSide(localPos) instanceof BackpackBlockEntity backpackBe) {
 			backpackBe.setBackpack(getStorageStack());
 			StructureTemplate.StructureBlockInfo blockInfo = abstractContraptionEntity.getContraption().getBlocks().get(localPos);
 			if (blockInfo != null && blockInfo.state().getBlock() instanceof BackpackBlock) {
 				backpackBe.setBlockState(blockInfo.state());
 			}
+			abstractContraptionEntity.getContraption().invalidateClientContraptionStructure();
 		}
 	}
 
