a2cc9eeb9e116f882cde5f2f00b0641262ae2a57
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-06-17 12:51:20 +0200

    fix: 🐛 Fixed storage GUI to properly close with Escape on single press after using any buttons

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpackscreateintegration/client/MountedBackpackScreen.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpackscreateintegration/client/MountedBackpackScreen.java
@@ -31,8 +31,9 @@ public class MountedBackpackScreen extends StorageScreenBase<MountedBackpackCont
 		if (isTextBoxFocused()) {
 			return super.keyPressed(event);
 		}
-		if (event.key() == 256 || KeybindHandler.BACKPACK_OPEN_KEYBIND.isActiveAndMatches(InputConstants.getKey(event))) {
-			if (getFocused() != null && !clearFocusedWidget()) {
+		boolean backpackKeyPressed = KeybindHandler.BACKPACK_OPEN_KEYBIND.isActiveAndMatches(InputConstants.getKey(event));
+		if (event.key() == 256 || backpackKeyPressed) {
+			if (event.key() != 256 && backpackKeyPressed && getFocused() != null && !clearFocusedWidget()) {
 				return super.keyPressed(event);
 			}
 			if (!getMenu().isFirstLevelStorage()) {
