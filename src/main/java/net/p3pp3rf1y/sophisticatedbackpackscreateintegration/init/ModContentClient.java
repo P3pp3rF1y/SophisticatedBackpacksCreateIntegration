b2d343518b19abc2d59e963bc6e29e497d097327
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-06-24 10:56:38 +0200

    chore: 🔧 spotless / pmd setup and a bit of cleanup

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpackscreateintegration/init/ModContentClient.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpackscreateintegration/init/ModContentClient.java
@@ -67,7 +67,8 @@ public class ModContentClient {
 		if (screen instanceof MountedBackpackScreen mountedBackpackScreen) {
 			Slot slot = mountedBackpackScreen.getSlotUnderMouse();
 
-			if (mountedBackpackScreen.getMenu().isFirstLevelStorage() && slot instanceof StorageInventorySlot && slot.getItem().getItem() instanceof BackpackItem && slot.getItem().getCount() == 1) {
+			if (mountedBackpackScreen.getMenu().isFirstLevelStorage() && slot instanceof StorageInventorySlot
+					&& slot.getItem().getItem() instanceof BackpackItem && slot.getItem().getCount() == 1) {
 				mountedBackpackScreen.getMenu().getContext().getSubBackpackContext(slot.index);
 				ClientPacketDistributor.sendToServer(new MountedSubBackpackOpenPayload(slot.index));
 				return true;
