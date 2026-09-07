287aea6439acb995eb16167a51a0b27522aeefc5
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-09-08 00:44:57 +0200

    chore: 🔧 Port linked storage to SB Create integration

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpackscreateintegration/backpack/MountedSophisticatedBackpackType.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpackscreateintegration/backpack/MountedSophisticatedBackpackType.java
@@ -15,6 +15,11 @@ public class MountedSophisticatedBackpackType extends MountedItemStorageType<Mou
 
 	@Override
 	public @Nullable MountedSophisticatedBackpack mount(Level level, BlockState state, BlockPos pos, @Nullable BlockEntity be) {
-		return be instanceof BackpackBlockEntity backpackBe ? MountedSophisticatedBackpack.from(backpackBe) : null;
+		if (!(be instanceof BackpackBlockEntity backpackBe)) {
+			return null;
+		}
+		MountedSophisticatedBackpack mountedBackpack = MountedSophisticatedBackpack.from(backpackBe);
+		mountedBackpack.initializeStorage(level);
+		return mountedBackpack;
 	}
 }
